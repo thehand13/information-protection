6896f4ccd4314edac8925cfca5d3ff684b366869
Roman <thehand@yandex.ru>
2023-04-23 22:44:53 +0300

    First lab is ready

--- src/components/labs/first/First.tsx
+++ src/components/labs/first/First.tsx
@@ -1,5 +1,77 @@
+import { FormEvent, useState } from "react";
+import styles from "../Labs.module.css";
+
+const RUSSIAN_KEYS = [
+  "А",
+  "Б",
+  "В",
+  "Г",
+  "Д",
+  "Е",
+  "Ё",
+  "Ж",
+  "З",
+  "И",
+  "Й",
+  "К",
+  "Л",
+  "М",
+  "Н",
+  "О",
+  "П",
+  "Р",
+  "С",
+  "Т",
+  "У",
+  "Ф",
+  "Х",
+  "Ц",
+  "Ш",
+  "Щ",
+  "Ъ",
+  "Ы",
+  "Ь",
+  "Э",
+  "Ю",
+  "Я",
+];
+
 const First = () => {
-  return <section></section>;
+  const [resultState, setResult] = useState("");
+  const [inputState, setInputState] = useState("");
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputState(event.target.value);
+  };
+
+  const onSubmitHandler = (event: FormEvent) => {
+    event.preventDefault();
+    let i = 0;
+    const resultArray = [];
+    while (i < inputState.length) {
+      const letter = inputState[i] + inputState[i + 1];
+      if (inputState[i] === "*" || inputState[i + 1] === "*") {
+        resultArray.push("");
+        i++;
+      } else {
+        resultArray.push(RUSSIAN_KEYS[+letter]);
+        i++;
+        i++;
+      }
+    }
+    setResult(resultArray.join(""));
+  };
+  return (
+    <section className={styles.lab}>
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" onChange={onChangeHandler} value={inputState} />
+        <button className={styles.calculate}>Calculate</button>
+      </form>
+      <div>
+        <b>Result is:</b> {resultState}
+      </div>
+    </section>
+  );
 };
 
 export default First;
