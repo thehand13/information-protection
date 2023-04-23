92cab0f3b8cd1f63b03ceef82d5889a3815445b4
Roman <thehand@yandex.ru>
2023-04-24 01:37:41 +0300

    Fourth is ready

--- src/components/labs/fourth/Fourth.tsx
+++ src/components/labs/fourth/Fourth.tsx
@@ -1,5 +1,45 @@
+import { FormEvent, useState } from "react";
+import styles from "../Labs.module.css";
+
+const LETTERS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
 const Fourth = () => {
-  return <></>;
+  const [resultState, setResult] = useState("");
+  const [inputState, setInputState] = useState("");
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputState(event.target.value);
+  };
+
+  const onSubmitHandler = (event: FormEvent) => {
+    event.preventDefault();
+
+    const resultArray: string[] = [];
+
+    const d = 5,
+      x1 = (-3 + Math.sqrt(d)) / 2,
+      x2 = (-3 - Math.sqrt(d)) / 2,
+      s1 = (((((x1 + 3) * x1 + 1) * x1 + 1) * x1 + 4) * x1 + 4) * x1 + 3,
+      s2 = (((((x2 + 3) * x2 + 1) * x2 + 1) * x2 + 4) * x2 + 4) * x2 + 3,
+      k = Math.floor(s1);
+    for (let i = 0; i < inputState.length; i++) {
+      if (LETTERS.indexOf(inputState[i])) {
+        resultArray.push(LETTERS[(LETTERS.indexOf(inputState[i]) - k) % 33]);
+      }
+    }
+    setResult(resultArray.join(""));
+  };
+  return (
+    <section className={styles.lab}>
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" onChange={onChangeHandler} value={inputState} />
+        <button className={styles.calculate}>Calculate</button>
+      </form>
+      <div>
+        <b>Result is:</b> {resultState}
+      </div>
+    </section>
+  );
 };
 
 export default Fourth;
